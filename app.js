0b1d944a4b66dc666c4c0ced577eefe3e7ecda9e
S. Ladawisalsup <30717573+S-Ladawisalsup@users.noreply.github.com>
2026-06-09 10:59:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,42 @@ const closeBtn = document.getElementById('closeBtn');
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
+
+const boxes = [
+  {
+    id: 1,
+    image: 'images/gift-box.png'
+  },
+  {
+    id: 2,
+    image: 'images/gift-box.png'
+  },
+  {
+    id: 3,
+    image: 'images/gift-box.png'
+  },
+  {
+    id: 4,
+    image: 'images/gift-box.png'
+  }
+];
+
+const container = document.getElementById('boxContainer');
+
+boxes.forEach(box => {
+
+  const card = document.createElement('div');
+
+  card.className = 'box-card';
+
+  card.innerHTML = `
+    <img src="${box.image}">
+  `;
+
+  card.addEventListener('click', () => {
+    alert(`เปิดกล่อง ${box.id}`);
+  });
+
+  container.appendChild(card);
+
+});
