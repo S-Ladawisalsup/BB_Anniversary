368070a6b099597be4f1e68cc186d10046dccaa5
S. Ladawisalsup <30717573+S-Ladawisalsup@users.noreply.github.com>
2026-06-09 10:49:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("App Loaded");
+const modal = document.getElementById('adModal');
+const closeBtn = document.getElementById('closeBtn');
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
